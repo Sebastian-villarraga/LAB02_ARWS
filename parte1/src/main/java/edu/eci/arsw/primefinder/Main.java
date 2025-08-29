f3a9a661711cabea501554f72402ce04cf9cc46c
Sebastian-villarraga <157900873+Sebastian-villarraga@users.noreply.github.com>
2025-08-28 22:12:10 -0500

    parte 1 punto 3

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -2,42 +2,79 @@ package edu.eci.arsw.primefinder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
         List<PrimeFinderThread> threads = new ArrayList<>();
+        Object lockObject = new Object();
+        boolean anyThreadRunning = true;
 
-        // Rango total a evaluar
-        int start = 0;
-        int end = 30000000;
-        int range = (end - start + 1) / 3;
+        threads.add(new PrimeFinderThread(0, 10000000, 1, lockObject));
+        threads.add(new PrimeFinderThread(10000001, 20000000, 2, lockObject));
+        threads.add(new PrimeFinderThread(20000001, 30000000, 3, lockObject));
 
-        // Crear tres hilos con rangos divididos en tercios
-        threads.add(new PrimeFinderThread(start, start + range - 1, 1));
-        threads.add(new PrimeFinderThread(start + range, start + 2 * range - 1, 2));
-        threads.add(new PrimeFinderThread(start + 2 * range, end, 3));
-
-        // Iniciar hilos
         for (PrimeFinderThread thread : threads) {
             thread.start();
         }
 
-        // Esperar a que terminen
+        try {
+            while (anyThreadRunning) {
+                anyThreadRunning = false;
+                for (PrimeFinderThread thread : threads) {
+                    if (thread.isAlive()) {
+                        anyThreadRunning = true;
+                        break;
+                    }
+                }
+                Thread.sleep(5000);
+                stopAndExecute(threads, lockObject);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Stops and executes threads
+     * @param threads List of threads
+     * @param lockObject Lock object to synchronize threads
+     * @throws InterruptedException
+     */
+
+    private static void stopAndExecute(List<PrimeFinderThread> threads, Object lockObject) throws InterruptedException {
         for (PrimeFinderThread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            thread.setExecution(false);
+        }
+        Thread.sleep(500);
+        System.out.println("--------------- stopping threads ---------------");
+        getTotalPrimes(threads);
+        System.out.println("Press ENTER to continue...");
+        sc.nextLine();
+        System.out.println("--------------- resuming threads ---------------");
+        for (PrimeFinderThread thread : threads) {
+            thread.setExecution(true);
+            synchronized (lockObject) {
+                lockObject.notifyAll();
             }
         }
+    }
 
-        // Mostrar total de primos
+    /**
+     * Gets total of primes found by threads
+     * @param threads List of threads
+     * @return Total of primes found by threads
+     */
+
+    private static void getTotalPrimes(List<PrimeFinderThread> threads) {
         int totalPrimes = 0;
         for (PrimeFinderThread thread : threads) {
             totalPrimes += thread.getPrimes().size();
         }
         System.out.println("Total of found primes: " + totalPrimes);
     }
+
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,16 +3,25 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The class PrimeFinderThread is a thread that finds prime numbers in a range
+ */
+
 public class PrimeFinderThread extends Thread {
 
     private int a, b, name;
-    private List<Integer> primes = new LinkedList<>();
+    private boolean execution;
+    private Object lockObject;
 
-    public PrimeFinderThread(int a, int b, int name) {
+    private List<Integer> primes = new LinkedList<Integer>();
+
+    public PrimeFinderThread(int a, int b, int name, Object lockObject) {
         super();
         this.a = a;
         this.b = b;
         this.name = name;
+        this.execution = true;
+        this.lockObject = lockObject;
     }
 
     public void run() {
@@ -20,13 +29,23 @@ public class PrimeFinderThread extends Thread {
             if (isPrime(i)) {
                 primes.add(i);
                 System.out.println("Thread " + name + " found prime: " + i);
+                synchronized (lockObject) {
+                    if (!execution) {
+                        try {
+                            System.out.println("Thread " + name + " is stoping...");
+                            lockObject.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
         }
     }
 
     boolean isPrime(int n) {
-        if (n < 2) return false;
-        if (n % 2 == 0 && n != 2) return false;
+        if (n % 2 == 0)
+            return false;
         for (int i = 3; i * i <= n; i += 2) {
             if (n % i == 0)
                 return false;
@@ -37,4 +56,9 @@ public class PrimeFinderThread extends Thread {
     public List<Integer> getPrimes() {
         return primes;
     }
+
+    public void setExecution(boolean execution) {
+        this.execution = execution;
+    }
+
 }
