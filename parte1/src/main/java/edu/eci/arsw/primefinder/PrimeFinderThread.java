467ccaf5e0144c6d58ae25746dfa84a9c0d64fb4
Sebastian-villarraga <157900873+Sebastian-villarraga@users.noreply.github.com>
2025-08-28 21:26:45 -0500

    parte 1 punto 2

--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,44 +3,59 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
 
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-		this.a = a;
-		this.b = b;
-	}
+public class PrimeFinderThread extends Thread {
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				System.out.println(i);
-			}
-		}
-		
-		
-	}
-	
-	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
-	}
+    private int a, b, name;
+    private boolean execution;
+    private Object lockObject;
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
-	
-	
-	
+    private List<Integer> primes = new LinkedList<Integer>();
+
+    public PrimeFinderThread(int a, int b, int name, Object lockObject) {
+        super();
+        this.a = a;
+        this.b = b;
+        this.name = name;
+        this.execution = true;
+        this.lockObject = lockObject;
+    }
+
+    public void run() {
+        for (int i = a; i <= b; i++) {
+            if (isPrime(i)) {
+                primes.add(i);
+                System.out.println("Thread " + name + " found prime: " + i);
+                synchronized (lockObject) {
+                    if (!execution) {
+                        try {
+                            System.out.println("Thread " + name + " is stoping...");
+                            lockObject.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    boolean isPrime(int n) {
+        if (n % 2 == 0)
+            return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    public void setExecution(boolean execution) {
+        this.execution = execution;
+    }
+
 }
